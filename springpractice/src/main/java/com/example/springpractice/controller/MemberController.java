ef7635e361b7c5735a69c80d133ebbef9e99474c
HanJeongmok <wjdahe1845@gmail.com>
2026-01-06 22:58:29 +0900

    스프링빈 등록

---
+++ springpractice/src/main/java/com/example/springpractice/controller/MemberController.java
@@ -0,0 +1,8 @@
+package com.example.springpractice.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class MemberController {
+    private final MemberService memberService = new MemberSer
+}
--- test-hand-on/src/main/java/com/example/test_hand_on/service/MemberService.java
+++ test-hand-on/src/main/java/com/example/test_hand_on/service/MemberService.java
@@ -3,13 +3,15 @@ package com.example.test_hand_on.service;
 import com.example.test_hand_on.domain.Member;
 import com.example.test_hand_on.repository.MemberRepository;
 import com.example.test_hand_on.repository.MemoryMemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
-
+@Service
 public class MemberService {
     private final MemberRepository memberRepository;
-
+    @Autowired
     public MemberService(MemberRepository memberRepository){
         this.memberRepository=memberRepository;
     }
